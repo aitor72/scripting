e170f7c7afdd26e7fb9806fc348bda03684f2fc7
Aitor <soy@aitor.me>
2020-04-22 10:20:50 +0200

    El bicho desaparece

---
+++ Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+
+	public float runSpeed; // 1
+public float gotHayDestroyDelay; // 2
+private bool hitByHay; // 3
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	transform.Translate(Vector3.down * runSpeed * Time.deltaTime);
+
+        
+    }
+
+    private void HitByHay()
+{
+    hitByHay = true; // 1
+    runSpeed = 0; // 2
+
+    Destroy(gameObject, gotHayDestroyDelay); // 3
+}
+
+private void OnTriggerEnter(Collider other) // 1
+{
+    if (other.CompareTag("Hay") && !hitByHay) // 2
+    {
+        Destroy(other.gameObject); // 3
+        HitByHay(); // 4
+    }
+
+
+}
+
+}
